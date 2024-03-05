aec84d2831ee5d0c71fe246ce46d80af57fbd28b
Ana Luiza Cerchiari de Andra <ana.andrade@salas.aulas>
2024-03-05 20:53:40 -0300

    app incompleta para 05-03

---
+++ app/api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+tarefas = [
+    {
+        "id": "1",
+        "item": "Escrever matéria revista."
+    },
+    {
+        "id": "2",
+        "item": "Revisar artigo jornal."
+    }
+]
+#nome da app
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+]
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_methods=["*"]
+)
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your tarefa list."}
+@app.get("/tarefa", tags=["tarefas"])
+async def get_tarefas() -> dict:
+    return { "data": tarefas }
+@app.post("/tarefa", tags=["tarefas"])
+async def add_tarefa(tarefinha: dict) -> dict:
+    tarefas.append(tarefinha)
+    return {
+        "data": { "Tarefa added." }
+    }
